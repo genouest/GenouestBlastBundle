55ec45c467b8822965ad85283f80a2cad044c250
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2011-07-01 14:38:30 +0200

    added split twig tag

---
+++ Extension/SplitExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Copyright 2011 Anthony Bretaudeau <abretaud@irisa.fr>
+ *
+ * Licensed under the CeCILL License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+namespace Genouest\Bundle\BlastBundle\Extension;
+
+class SplitExtension extends \Twig_Extension {
+
+    public function getFilters() {
+        return array(
+            'split'   => new \Twig_Filter_Method($this, 'twig_split_str'),
+        );
+    }
+
+    public function getName()
+    {
+        return 'split';
+    }
+
+    public function twig_split_str($value, $glue = ',') {
+        return explode($glue, $value);
+    }
+
+}
